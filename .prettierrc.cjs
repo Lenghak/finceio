bd731e6404570f583d825c357e4bff3c0dc4aa7a
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-08-13 14:39:51 +0700

    chore(config): added development configuration

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  htmlWhitespaceSensitivity: "css",
+  jsxSingleQuote: false,
+  singleAttributePerLine: true,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
