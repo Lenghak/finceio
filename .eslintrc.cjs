c6b9fd04c820bd51053044ab9fc9d53407b2b972
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-08-15 13:40:11 +0700

    chore(config): moved to .cjs files & config eslint + prettier + ts

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.eslint.json", "./packages/*/tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 /** @type {import("prettier").Config} */
-export default {
+module.exports = {
   arrowParens: "always",
   bracketSpacing: true,
   bracketSameLine: false,
