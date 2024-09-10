2871b1e092a635b9ab7b2aeba5507832545e41dc
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-09-10 13:55:29 +0000

    feat(ui): avoided export * in package/ui

--- packages/ui/shadcn.ts
+++ packages/ui/shadcn.ts
@@ -1,65 +1,320 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-export * from "./src/components/shadcn-ui/accordion";
-export * from "./src/components/shadcn-ui/alert";
-export * from "./src/components/shadcn-ui/alert-dialog";
-export * from "./src/components/shadcn-ui/aspect-ratio";
-export * from "./src/components/shadcn-ui/avatar";
+export {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./src/components/shadcn-ui/accordion";
+export {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "./src/components/shadcn-ui/alert";
+export {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./src/components/shadcn-ui/alert-dialog";
+export { AspectRatio } from "./src/components/shadcn-ui/aspect-ratio";
+export {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "./src/components/shadcn-ui/avatar";
 
-export * from "./src/components/shadcn-ui/badge";
-export * from "./src/components/shadcn-ui/breadcrumb";
-export * from "./src/components/shadcn-ui/button";
+export {
+  Badge,
+  type BadgeProps,
+  badgeVariants,
+} from "./src/components/shadcn-ui/badge";
+export {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "./src/components/shadcn-ui/breadcrumb";
+export {
+  Button,
+  type ButtonProps,
+  buttonVariants,
+} from "./src/components/shadcn-ui/button";
 
-export * from "./src/components/shadcn-ui/calendar";
-export * from "./src/components/shadcn-ui/card";
-export * from "./src/components/shadcn-ui/carousel";
-export * from "./src/components/shadcn-ui/chart";
-export * from "./src/components/shadcn-ui/checkbox";
-export * from "./src/components/shadcn-ui/collapsible";
-export * from "./src/components/shadcn-ui/command";
-export * from "./src/components/shadcn-ui/context-menu";
+export {
+  Calendar,
+  type CalendarProps,
+} from "./src/components/shadcn-ui/calendar";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./src/components/shadcn-ui/card";
+export {
+  Carousel,
+  type CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./src/components/shadcn-ui/carousel";
+export {
+  ChartTooltipContent,
+  type ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartStyle,
+  ChartTooltip,
+} from "./src/components/shadcn-ui/chart";
+export { Checkbox } from "./src/components/shadcn-ui/checkbox";
+export {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./src/components/shadcn-ui/collapsible";
+export {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "./src/components/shadcn-ui/command";
+export {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuGroup,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuPortal,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "./src/components/shadcn-ui/context-menu";
 
-export * from "./src/components/shadcn-ui/dialog";
-export * from "./src/components/shadcn-ui/drawer";
-export * from "./src/components/shadcn-ui/dropdown-menu";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./src/components/shadcn-ui/dialog";
+export {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./src/components/shadcn-ui/drawer";
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./src/components/shadcn-ui/dropdown-menu";
 
-export * from "./src/components/shadcn-ui/form";
+export {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from "./src/components/shadcn-ui/form";
 
-export * from "./src/components/shadcn-ui/hover-card";
+export {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "./src/components/shadcn-ui/hover-card";
 
-export * from "./src/components/shadcn-ui/input";
-export * from "./src/components/shadcn-ui/input-otp";
+export { Input, type InputProps } from "./src/components/shadcn-ui/input";
+export {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "./src/components/shadcn-ui/input-otp";
 
-export * from "./src/components/shadcn-ui/label";
+export { Label } from "./src/components/shadcn-ui/label";
 
-export * from "./src/components/shadcn-ui/menubar";
+export {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarGroup,
+  MenubarItem,
+  MenubarLabel,
+  MenubarMenu,
+  MenubarPortal,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "./src/components/shadcn-ui/menubar";
 
-export * from "./src/components/shadcn-ui/navigation-menu";
+export {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "./src/components/shadcn-ui/navigation-menu";
 
-export * from "./src/components/shadcn-ui/pagination";
-export * from "./src/components/shadcn-ui/popover";
-export * from "./src/components/shadcn-ui/progress";
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./src/components/shadcn-ui/pagination";
+export {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "./src/components/shadcn-ui/popover";
+export { Progress } from "./src/components/shadcn-ui/progress";
 
-export * from "./src/components/shadcn-ui/radio-group";
-export * from "./src/components/shadcn-ui/resizable";
+export {
+  RadioGroup,
+  RadioGroupItem,
+} from "./src/components/shadcn-ui/radio-group";
+export {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./src/components/shadcn-ui/resizable";
 
-export * from "./src/components/shadcn-ui/scroll-area";
-export * from "./src/components/shadcn-ui/select";
-export * from "./src/components/shadcn-ui/separator";
-export * from "./src/components/shadcn-ui/sheet";
-export * from "./src/components/shadcn-ui/skeleton";
-export * from "./src/components/shadcn-ui/slider";
+export { ScrollArea, ScrollBar } from "./src/components/shadcn-ui/scroll-area";
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./src/components/shadcn-ui/select";
+export { Separator } from "./src/components/shadcn-ui/separator";
+export {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetOverlay,
+  SheetPortal,
+  SheetTitle,
+  SheetTrigger,
+} from "./src/components/shadcn-ui/sheet";
+export { Skeleton } from "./src/components/shadcn-ui/skeleton";
+export { Slider } from "./src/components/shadcn-ui/slider";
 export { Toaster as SonnerToaster } from "./src/components/shadcn-ui/sonner";
-export * from "./src/components/shadcn-ui/switch";
+export { Switch } from "./src/components/shadcn-ui/switch";
 
-export * from "./src/components/shadcn-ui/table";
-export * from "./src/components/shadcn-ui/tabs";
-export * from "./src/components/shadcn-ui/textarea";
-export * from "./src/components/shadcn-ui/toast";
-export * from "./src/components/shadcn-ui/toaster";
-export * from "./src/components/shadcn-ui/toggle";
-export * from "./src/components/shadcn-ui/toggle-group";
-export * from "./src/components/shadcn-ui/tooltip";
+export {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./src/components/shadcn-ui/table";
+export {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "./src/components/shadcn-ui/tabs";
+export {
+  Textarea,
+  type TextareaProps,
+} from "./src/components/shadcn-ui/textarea";
+export {
+  Toast,
+  ToastAction,
+  type ToastActionElement,
+  ToastClose,
+  ToastDescription,
+  type ToastProps,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "./src/components/shadcn-ui/toast";
+export { Toaster } from "./src/components/shadcn-ui/toaster";
+export { Toggle, toggleVariants } from "./src/components/shadcn-ui/toggle";
+export {
+  ToggleGroup,
+  ToggleGroupItem,
+} from "./src/components/shadcn-ui/toggle-group";
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./src/components/shadcn-ui/tooltip";
 
-export * from "./src/components/shadcn-ui/use-toast";
+export { reducer, toast, useToast } from "./src/components/shadcn-ui/use-toast";
 
-export * from "./src/lib/utils";
+export { cn } from "./src/lib/utils";
