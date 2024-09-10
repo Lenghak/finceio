32447d63308ba4ea262b205416337f4429b8ffae
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-09-10 15:03:28 +0000

    chore(packages/lib): added http-client data fetching lib

---
+++ packages/lib/http-client.ts
@@ -0,0 +1,23 @@
+import httpClient from "ky";
+
+export { httpClient };
+export {
+  type AfterResponseHook,
+  type BeforeErrorHook,
+  type BeforeRequestHook,
+  type BeforeRetryHook,
+  type BeforeRetryState,
+  type DownloadProgress,
+  HTTPError,
+  type Hooks,
+  type Input,
+  type KyInstance,
+  type KyRequest,
+  type KyResponse,
+  type NormalizedOptions,
+  type Options,
+  type ResponsePromise,
+  type RetryOptions,
+  type SearchParamsOption,
+  TimeoutError,
+} from "ky";
