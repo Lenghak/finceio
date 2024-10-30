a77db8515b154e275c6cc6113b6092a3bebbffe9
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-10-30 22:14:08 +0700

    feat(@frontend/web): added hono

---
+++ frontend/web/src/app/fonts.tsx
@@ -0,0 +1,30 @@
+import {
+  Playfair_Display,
+  Source_Code_Pro,
+  Source_Sans_3,
+  Source_Serif_4,
+} from "next/font/google"
+
+export const sans = Source_Sans_3({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+})
+
+export const serif = Source_Serif_4({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+})
+
+export const mono = Source_Code_Pro({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+})
+
+export const display = Playfair_Display({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+})
---
+++ frontend/web/src/providers/queryclient-provider.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { type PropsWithChildren, useMemo, useState } from "react"
+
+import { getQueryClient } from "@/lib/queryclient"
+
+// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
+import { QueryClientProvider as QueryClientPrimitiveProvider } from "@tanstack/react-query"
+
+export function QueryClientProvider({ children }: PropsWithChildren) {
+  // NOTE: Avoid useState when initializing the query client if you don't
+  //       have a suspense boundary between this and the code that may
+  //       suspend because React will throw away the client on the initial
+  //       render if it suspends and there is no boundary
+  const [queryClient] = useState(getQueryClient())
+
+  const provider = useMemo(
+    () => (
+      <QueryClientPrimitiveProvider client={queryClient}>
+        {children}
+      </QueryClientPrimitiveProvider>
+    ),
+    [queryClient, children],
+  )
+
+  return provider
+}
