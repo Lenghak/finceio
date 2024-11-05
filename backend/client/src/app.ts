83820f7d6826872f7fbbb330fc2572c8414f6d49
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-11-05 17:32:19 +0700

    feat: init structures

--- backend/client/src/app.ts
+++ backend/client/src/app.ts
@@ -1,6 +1,21 @@
 import { Hono } from "hono";
+import { compress } from "hono/compress";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { secureHeaders } from "hono/secure-headers";
+import { trimTrailingSlash } from "hono/trailing-slash";
 
 const app = new Hono();
+
+app.use(compress());
+app.use(cors());
+app.use(csrf());
+app.use(logger());
+app.use(secureHeaders());
+app.use(trimTrailingSlash());
+app.use(prettyJSON());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
