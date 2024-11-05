3379316e481502a59d76ed72cac333dc55067918
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-11-05 17:31:56 +0700

    feat: init auth

--- frontend/web/auth.config.ts
+++ frontend/web/auth.config.ts
@@ -1,6 +1,24 @@
+import type { Provider } from "@auth/core/providers";
+import Credentials from "@auth/core/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  providers: [],
+  providers: [
+    Credentials({
+      authorize: (credentials, _req) => {
+        const { provider: _provider, email: _email } = credentials as {
+          provider: Provider;
+          email: string | undefined | null;
+        };
+
+        // Magic Link Sign In
+
+        // OAuth Sign In
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {},
   session: { strategy: "jwt" },
 } satisfies NextAuthConfig;
--- frontend/web/src/app/api/[[...route]]/route.ts
+++ frontend/web/src/app/api/[[...route]]/route.ts
@@ -3,10 +3,22 @@ import authConfig from "~/auth.config";
 import { authHandler, initAuthConfig } from "@hono/auth-js";
 import { Hono } from "hono";
 import { env } from "hono/adapter";
+import { compress } from "hono/compress";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
+import { prettyJSON } from "hono/pretty-json";
+import { secureHeaders } from "hono/secure-headers";
+import { trimTrailingSlash } from "hono/trailing-slash";
 import { handle } from "hono/vercel";
 
 const app = new Hono();
 
+app.use(cors());
+app.use(compress());
+app.use(csrf());
+app.use(secureHeaders());
+app.use(trimTrailingSlash());
+app.use(prettyJSON());
 app.use(
   "*",
   initAuthConfig((c) => ({
