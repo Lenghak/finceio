87d2ef06b96b1d55c553d2728842240a98a9b5ec
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-11-16 18:47:26 +0700

    build: fix env schema validation

--- frontend/web/src/env.ts
+++ frontend/web/src/env.ts
@@ -1,46 +1,37 @@
-import { url, type InferInput, object, pipe, string } from "valibot";
+import { url, object, parse, pipe, string } from "valibot";
 
 const serverSchema = object({
-  AUTH_URL: pipe(string(), url("AUTH_SECRET")),
+  AUTH_URL: pipe(string("AUTH_SECRET"), url("AUTH_SECRET")),
   AUTH_SECRET: string("AUTH_SECRET"),
 });
 
 const clientSchema = object({
-  NEXT_PUBLIC_ORIGIN_URL: pipe(string(), url("NEXT_PUBLIC_ORIGIN_URL")),
-  NEXT_PUBLIC_API_URL: pipe(string(), url("NEXT_PUBLIC_API_URL")),
+  NEXT_PUBLIC_ORIGIN_URL: pipe(
+    string("NEXT_PUBLIC_ORIGIN_URL"),
+    url("NEXT_PUBLIC_ORIGIN_URL"),
+  ),
+  NEXT_PUBLIC_API_URL: pipe(
+    string("NEXT_PUBLIC_ORIGIN_URL"),
+    url("NEXT_PUBLIC_API_URL"),
+  ),
 });
 
-const serverEnv = (
+const serverEnv =
   typeof window === "undefined"
-    ? serverSchema["~run"](
-        {
-          value: {
-            AUTH_URL: process.env.AUTH_URL,
-            AUTH_SECRET: process.env.AUTH_SECRET,
-          },
-        },
-        {
-          message: (issues) => {
-            return issues.message;
-          },
-        },
-      ).value
-    : {}
-) as InferInput<typeof serverSchema>;
+    ? parse(serverSchema, {
+        AUTH_URL: process.env.AUTH_URL,
+        AUTH_SECRET: process.env.AUTH_SECRET,
+      })
+    : (() => {
+        process.env.AUTH_URL = undefined;
+        process.env.AUTH_SECRET = undefined;
+        return process.env;
+      })();
 
-const clientEnv = clientSchema["~run"](
-  {
-    value: {
-      NEXT_PUBLIC_ORIGIN_URL: process.env.NEXT_PUBLIC_ORIGIN_URL,
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    },
-  },
-  {
-    message: (issues) => {
-      return issues.message;
-    },
-  },
-).value as InferInput<typeof clientSchema>;
+const clientEnv = parse(clientSchema, {
+  NEXT_PUBLIC_ORIGIN_URL: process.env.NEXT_PUBLIC_ORIGIN_URL,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+});
 
 export const env = {
   ...clientEnv,
--- frontend/web/src/providers/session-provider.tsx
+++ frontend/web/src/providers/session-provider.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { SessionProvider as NextAuthSessionProvider } from "@hono/auth-js/react";
+import { useMemo } from "react";
 import type { PropsWithChildren } from "react";
 
 export function SessionProvider({ children }: PropsWithChildren) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+  return useMemo(
+    () => <NextAuthSessionProvider>{children}</NextAuthSessionProvider>,
+    [children],
+  );
 }
