804430cd39af1e1b3039b7392d6723c9594dc335
Lenghak Hok <lenghakhok.dev@gmail.com>
2024-11-20 10:49:14 +0700

    chore(build): added oauth dependencies

--- backend/client/src/shared/env.ts
+++ backend/client/src/shared/env.ts
@@ -1,7 +1,19 @@
-import { object } from "valibot";
+import { email, number, object, parse, pipe, string, transform } from "valibot";
 
-const envSchema = object({});
+const envSchema = object({
+  APP_SERVER_PORT: pipe(
+    string("APP_SERVER_PORT"),
+    transform((v) => number(v)),
+  ),
 
-export const env = envSchema["~validate"]({
-  value: {},
+  EMAIL_SERVER_USER: string("EMAIL_SERVER_USER"),
+  EMAIL_SERVER_PASSWORD: string("EMAIL_SERVER_PASSWORD"),
+  EMAIL_SERVER_HOST: string("EMAIL_SERVER_HOST"),
+  EMAIL_SERVER_PORT: pipe(
+    string("EMAIL_SERVER_PORT"),
+    transform((v) => number(v)),
+  ),
+  EMAIL_FROM: pipe(string("EMAIL_FROM"), email("EMAIL_FROM")),
 });
+
+export const env = parse(envSchema, {});
